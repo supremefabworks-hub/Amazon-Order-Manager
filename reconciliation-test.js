16435ecbd29d583493d968d4399127ff3a5cd8f0
SFW <supremefabworks@gmail.com>
2026-09-03 15:51:29 -0700

    Remove stale hardcoded release version from reconciliation test

--- reconciliation-test.js
+++ reconciliation-test.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 
 const manifest = JSON.parse(fs.readFileSync(__dirname + '/manifest.json', 'utf8'));
-if (manifest.version !== '0.17.0') throw new Error('manifest version mismatch');
+const pkg = JSON.parse(fs.readFileSync(__dirname + '/package.json', 'utf8'));
+if (manifest.version !== pkg.version) throw new Error('manifest/package version mismatch');
 
 const dashboard = fs.readFileSync(__dirname + '/dashboard.js', 'utf8');
 for (const token of [
